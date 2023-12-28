aea96a017b0903e2bb1df71c766ef5bab1fdbf35
sleeping_dx <sleeping_dx@hotmail.com>
2023-12-28 23:46:07 +0800

    1. 一个格子10个像素

--- codes/MyDefine.py
+++ codes/MyDefine.py
@@ -1,3 +1,3 @@
 GAME_NAME = 'Extreme Survival'  # name of the game
 GAME_RESOLUTION = (1024, 800)  # resolution of the game
-GAME_GRID = 10  # size of a grid (px)
+MAP_GRID = 10  # size of a grid (px)
