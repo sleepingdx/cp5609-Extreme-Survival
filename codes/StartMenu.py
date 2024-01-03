806191bb16e59145acf44679ed0fd6ab0fd42ae9
myx <myx971212@outlook.com>
2024-01-03 12:54:13 +0800

    1.增加开始界面和角色选择界面 2.更改背景，字体颜色，字体大小

--- codes/StartMenu.py
+++ codes/StartMenu.py
@@ -19,11 +19,17 @@ black = (0, 0, 0)
 green = (0, 255, 0)
 
 # 字体设置为默认字体
-font = pygame.font.Font(None, 36)
+font_button = pygame.font.Font(None, 36)
+font_text = pygame.font.Font(None, 100)
 
 # 函数：显示文本
-def draw_text(text, x, y, color=black):
-    text_surface = font.render(text, True, color)
+def draw_text(text, x, y, color=white):
+    text_surface = font_text.render(text, True, color)
+    text_rect = text_surface.get_rect(center=(x, y))
+    screen.blit(text_surface, text_rect)
+
+def draw_button(text, x, y, color=black):
+    text_surface = font_button.render(text, True, color)
     text_rect = text_surface.get_rect(center=(x, y))
     screen.blit(text_surface, text_rect)
 
@@ -55,18 +61,18 @@ while running:
     screen.blit(background_image, (0, 0))
 
     # 显示标题
-    draw_text("Extreme Survival", width // 2, 100)
+    draw_text("Extreme Survival", width // 2, 120)
 
     # 创建按钮
     start_button_rect = pygame.Rect(width // 2 - 100, 300, 200, 50)
     pygame.draw.rect(screen, green, start_button_rect)
 
     # 显示按钮上的文字
-    draw_text("Start", start_button_rect.centerx, start_button_rect.centery, black)
+    draw_button("Start", start_button_rect.centerx, start_button_rect.centery, black)
 
     exit_button_rect = pygame.Rect(width // 2 - 100, 400, 200, 50)
     pygame.draw.rect(screen, green, exit_button_rect)
-    draw_text("Exit", width // 2, 425, black)
+    draw_button("Exit", width // 2, 425, black)
 
     # 更新屏幕
     pygame.display.flip()
