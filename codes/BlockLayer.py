b28d50827b575f0b5399420ca4f5e3d8d5bd6299
sleeping_dx <sleeping_dx@hotmail.com>
2024-01-08 12:12:33 +0800

    1. 九宫格的object改成数组

--- codes/BlockLayer.py
+++ codes/BlockLayer.py
@@ -12,10 +12,16 @@ class BlockLayer(Singleton):
         self.m_objects = {}
         for row in range(len(self.m_blocks)):
             for col in range(len(self.m_blocks[row])):
-                self.m_objects[f'{row},{col}'] = None
+                self.m_objects[f'{row},{col}'] = []
 
-    def hold_place(self, row, col, obj, enum):
-        if self.m_blocks[row][col] and self.m_blocks[row][col] == 0:
+    def append(self, row, col, obj, enum):
+        if self.m_blocks[row][col] and self.m_blocks[row][col] == MyDefine.BLOCK_PLACEHOLDERS[0]:
             self.m_blocks[row][col] = enum
             if enum == MyDefine.BLOCK_PLACEHOLDERS[2]:
-                self.m_objects[f'{row},{col}'] = obj
+                self.m_objects[f'{row},{col}'].append(obj)
+
+    def delete(self, row, col, obj, enum):
+        if self.m_blocks[row][col] and self.m_blocks[row][col] == MyDefine.BLOCK_PLACEHOLDERS[2]:
+            self.m_blocks[row][col] = enum
+            if enum == MyDefine.BLOCK_PLACEHOLDERS[0]:
+                self.m_objects[f'{row},{col}'].remove(obj)
--- codes/FSM/State.py
+++ codes/FSM/State.py
@@ -18,15 +18,11 @@ class State:
         objects = BlockLayer.get_instance().m_objects
         if self.m_row != row or self.m_col != col:
             if self.m_row != MyDefine.INVALID_ID and self.m_col != MyDefine.INVALID_ID:
-                if blocks[self.m_row][self.m_col] == MyDefine.BLOCK_PLACEHOLDERS[2]:
-                    blocks[self.m_row][self.m_col] = MyDefine.BLOCK_PLACEHOLDERS[0]
-                    objects[f'{self.m_row},{self.m_col}'] = None
+                BlockLayer.get_instance().delete(self.m_row, self.m_col, self.m_object, MyDefine.BLOCK_PLACEHOLDERS[0])
             self.m_row = row
             self.m_col = col
             if 0 <= self.m_row < len(blocks) and 0 <= self.m_col < len(blocks[self.m_row]):
-                if blocks[self.m_row][self.m_col] == MyDefine.BLOCK_PLACEHOLDERS[0]:
-                    blocks[self.m_row][self.m_col] = MyDefine.BLOCK_PLACEHOLDERS[2]
-                    objects[f'{self.m_row},{self.m_col}'] = self.m_object
+                BlockLayer.get_instance().append(self.m_row, self.m_col, self.m_object, MyDefine.BLOCK_PLACEHOLDERS[2])
 
     def end(self):
         pass
