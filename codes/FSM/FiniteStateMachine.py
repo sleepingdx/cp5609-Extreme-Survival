af736ed09387f999754d5b15a38717767f462c60
sleeping_dx <sleeping_dx@hotmail.com>
2024-01-12 01:01:11 +0800

    1. 修改主角默认出生点

--- codes/FSM/FiniteStateMachine.py
+++ codes/FSM/FiniteStateMachine.py
@@ -2,15 +2,17 @@ from codes.JsonManager import JsonManager
 from codes.FSM.Idle import Idle
 from codes.FSM.Move import Move
 from codes.FSM.Attack import Attack
+from codes.FSM.Injured import Injured
 from codes.FSM.Dead import Dead
 
-ENUM_FSM_STATES = ["Idle", "Move", "Attack", "Dead"]
+ENUM_FSM_STATES = ["Idle", "Move", "Attack", "Injured", "Dead"]
 
 
 class FiniteStateMachine:
     def __init__(self, obj, fsm_type):
         self.m_states = {}
         self.m_current = 0
+        self.m_last_state = self.m_current
         # Initialize states
         self.m_fsm = JsonManager.get_instance().m_json_fsm[fsm_type]
         for key in self.m_fsm.keys():
@@ -23,5 +25,6 @@ class FiniteStateMachine:
     def change_status(self, index):
         if index in self.m_fsm[ENUM_FSM_STATES[self.m_current]] and index != ENUM_FSM_STATES[self.m_current]:
             self.m_states[ENUM_FSM_STATES[self.m_current]].end()
+            self.m_last_state = self.m_current
             self.m_current = index
             self.m_states[ENUM_FSM_STATES[self.m_current]].begin()
