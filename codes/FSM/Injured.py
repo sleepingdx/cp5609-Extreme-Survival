2e51e2070bc7dfb2b950dd8df34b2d05afae3fe7
sleeping_dx <sleeping_dx@hotmail.com>
2024-01-12 12:24:51 +0800

    Revert "1. 修改攻击完毕之后保持原有的状态 2. 修改受到攻击后, 如果没有死亡的情况下, 保持原有的状态"
    This reverts commit 6a36af51

--- codes/FSM/Injured.py
+++ codes/FSM/Injured.py
@@ -15,9 +15,8 @@ class Injured(State):
         if self.m_object.is_action_completed(self.m_object.m_current):
             if self.m_object.m_hp <= 0:
                 self.m_object.onDied()
-            else:
-                if not self.m_object.m_fsm.change_status(self.m_object.m_fsm.m_last_state):
-                    self.m_object.m_fsm.change_status(0)
+            elif self.m_object.m_fsm.m_last_state != 3:
+                self.m_object.m_fsm.change_status(self.m_object.m_fsm.m_last_state)
 
     def end(self):
         super().end()
