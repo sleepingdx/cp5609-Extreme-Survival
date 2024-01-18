cab5ffde34ecbb91fb230ffe037e74e8c04246db
sleeping_dx <sleeping_dx@hotmail.com>
2024-01-19 02:34:10 +0800

    1. 修复受击动作不显示的问题

--- codes/FSM/Injured.py
+++ codes/FSM/Injured.py
@@ -16,8 +16,9 @@ class Injured(State):
         if self.m_object.m_hp <= 0:
             self.m_object.onDied()
         else:
-            if not self.m_object.m_fsm.change_state(self.m_object.m_fsm.m_last_state):
-                self.m_object.m_fsm.change_state(0, self.m_arg1)
+            if self.m_object.is_action_completed(self.m_object.m_current):
+                if not self.m_object.m_fsm.change_state(self.m_object.m_fsm.m_last_state):
+                    self.m_object.m_fsm.change_state(0, self.m_arg1)
 
     def end(self):
         return super().end()
