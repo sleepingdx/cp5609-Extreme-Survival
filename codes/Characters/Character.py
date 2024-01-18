34d9c35488c28ce2c92aa6ff4bd4f96c47896402
sleeping_dx <sleeping_dx@hotmail.com>
2024-01-18 22:59:32 +0800

    1. 修复预碰撞后的状态

--- codes/Characters/Character.py
+++ codes/Characters/Character.py
@@ -130,14 +130,14 @@ class Character:
                     cur_col = int(cls.m_path[cls.m_current].x // MyDefine.BLOCK_RESOLUTION[1])
                     if blocks[cur_row][cur_col] != MyDefine.BLOCK_PLACEHOLDERS[0] and len(
                             objects[f'{cur_row},{cur_col}']) > 1:
-                        self.m_fsm.change_state(self.m_fsm.m_current)
+                        self.m_fsm.change_state(self.m_fsm.m_last_state)
                         print("Current point is blocked, recalculate a new route.")
                         return False
                     # The next grid is blocked. Need to recalculate a new route.
                     next_row = int(cls.m_path[cls.m_current + 1].z // MyDefine.BLOCK_RESOLUTION[0])
                     next_col = int(cls.m_path[cls.m_current + 1].x // MyDefine.BLOCK_RESOLUTION[1])
                     if blocks[next_row][next_col] != MyDefine.BLOCK_PLACEHOLDERS[0]:
-                        self.m_fsm.change_state(self.m_fsm.m_current)
+                        self.m_fsm.change_state(self.m_fsm.m_last_state)
                         print("Next point is blocked, recalculate a new route.")
                         return False
                 # Velocity
