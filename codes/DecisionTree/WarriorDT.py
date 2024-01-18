f2300b5614dd2e91ac56207ddcc6684e3166578d
sleeping_dx <sleeping_dx@hotmail.com>
2024-01-18 15:07:06 +0800

    1. 再次修改状态机规则. 2. 决策树逻辑仅在idle和patrol下进行.

--- codes/DecisionTree/WarriorDT.py
+++ codes/DecisionTree/WarriorDT.py
@@ -14,50 +14,53 @@ class WarriorDT(BasicDT):
 
     def update(self):
         super().update()
-        # Find the nearest enemy
-        blocks = BlockLayer.get_instance().m_blocks
-        row_left = max(0, int((self.m_object.m_position.z - self.m_object.m_search_enemy_scope * MyDefine.MAP_GRID) //
-                              MyDefine.BLOCK_RESOLUTION[0]))
-        row_right = min(len(blocks) - 1,
-                        int((self.m_object.m_position.z + self.m_object.m_search_enemy_scope * MyDefine.MAP_GRID) //
-                            MyDefine.BLOCK_RESOLUTION[0]))
-        col_top = max(0, int((self.m_object.m_position.x - self.m_object.m_search_enemy_scope * MyDefine.MAP_GRID) //
-                             MyDefine.BLOCK_RESOLUTION[1]))
-        col_bottom = min(len(blocks[row_left]) - 1,
-                         int((self.m_object.m_position.x + self.m_object.m_search_enemy_scope * MyDefine.MAP_GRID) //
-                             MyDefine.BLOCK_RESOLUTION[1]))
-        final_distance = None
-        target = None
-        for row in range(row_left, row_right + 1):
-            for col in range(col_top, col_bottom + 1):
-                objects = BlockLayer.get_instance().m_objects[f'{row},{col}']
-                for i in range(len(objects)):
-                    if self.m_object != objects[i] and objects[i].m_hp > 0 and objects[i].m_type in TARGET_TYPES:
-                        distance = (objects[i].m_position - self.m_object.m_position).calculate_magnitude2()
-                        if not final_distance:
-                            final_distance = distance
-                        elif distance <= final_distance:
-                            final_distance = distance
-                        if distance <= (self.m_object.m_search_enemy_scope * MyDefine.MAP_GRID) ** 2:
-                            target = objects[i]
+        if self.m_object.m_fsm.m_current in (0, 2):
+            # Find the nearest enemy
+            blocks = BlockLayer.get_instance().m_blocks
+            row_left = max(0,
+                           int((self.m_object.m_position.z - self.m_object.m_search_enemy_scope * MyDefine.MAP_GRID) //
+                               MyDefine.BLOCK_RESOLUTION[0]))
+            row_right = min(len(blocks) - 1,
+                            int((self.m_object.m_position.z + self.m_object.m_search_enemy_scope * MyDefine.MAP_GRID) //
+                                MyDefine.BLOCK_RESOLUTION[0]))
+            col_top = max(0,
+                          int((self.m_object.m_position.x - self.m_object.m_search_enemy_scope * MyDefine.MAP_GRID) //
+                              MyDefine.BLOCK_RESOLUTION[1]))
+            col_bottom = min(len(blocks[row_left]) - 1,
+                             int((self.m_object.m_position.x + self.m_object.m_search_enemy_scope *
+                                  MyDefine.MAP_GRID) // MyDefine.BLOCK_RESOLUTION[1]))
+            final_distance = None
+            target = None
+            for row in range(row_left, row_right + 1):
+                for col in range(col_top, col_bottom + 1):
+                    objects = BlockLayer.get_instance().m_objects[f'{row},{col}']
+                    for i in range(len(objects)):
+                        if self.m_object != objects[i] and objects[i].m_hp > 0 and objects[i].m_type in TARGET_TYPES:
+                            distance = (objects[i].m_position - self.m_object.m_position).calculate_magnitude2()
+                            if not final_distance:
+                                final_distance = distance
+                            elif distance <= final_distance:
+                                final_distance = distance
+                            if distance <= (self.m_object.m_search_enemy_scope * MyDefine.MAP_GRID) ** 2:
+                                target = objects[i]
 
-        # Decision Tree
-        if self.m_object.m_hp / self.m_object.m_max_hp < 50 / 100:
-            if target:
-                self.change_state(4, target)
-            else:
-                self.change_state(0, None)
-        else:
-            if target:
-                if final_distance <= (self.m_object.m_attack_enemy_scope * MyDefine.MAP_GRID) ** 2:
-                    self.change_state(5, target)
+            # Decision Tree
+            if self.m_object.m_hp / self.m_object.m_max_hp < 50 / 100:
+                if target:
+                    self.change_state(4, target)
                 else:
-                    self.change_state(3, target)
-            else:
-                if self.m_object.m_hp < self.m_object.m_max_hp:
                     self.change_state(0, None)
+            else:
+                if target:
+                    if final_distance <= (self.m_object.m_attack_enemy_scope * MyDefine.MAP_GRID) ** 2:
+                        self.change_state(5, target)
+                    else:
+                        self.change_state(3, target)
                 else:
-                    self.change_state(2, None)
+                    if self.m_object.m_hp < self.m_object.m_max_hp:
+                        self.change_state(0, None)
+                    else:
+                        self.change_state(2, None)
 
     def end(self):
         super().end()
